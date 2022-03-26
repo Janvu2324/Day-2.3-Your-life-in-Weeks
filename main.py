5d4aabd69f3a197ba91ec73853299440a0f24727
Janvi Patel <37372222+Janvu2324@users.noreply.github.com>
2022-03-26 07:17:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+left_year = 90 - int(age)
+days = 365 * left_year
+weeks = 52 * left_year
+months = 12 * left_year
+
+print(f'You have {days} days , {weeks} weeks and  {months} months left.')
+
+
+
+
+
+
+
+
+
